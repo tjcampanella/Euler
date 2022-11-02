9c176a7c6c7118aace015f66db011014a878c3b8
Tj Campanella <tj@arete.health>
2022-11-01 23:02:47 -0400

    worked on e11

--- e11/src/e11/core.clj
+++ e11/src/e11/core.clj
@@ -1,5 +1,13 @@
-(ns e11.core "Largest Product in a Grid")
+(ns e11.core "Largest Product in a Grid"
+    (:require [clojure.string :as str]))
 
+(defn convert-row-to-int [row]
+  (mapv #(Integer/parseInt %) row))
+
+(def data (->> (-> (slurp "data.txt")
+                   (str/split-lines))
+               (mapv #(str/split % #" "))
+               (mapv convert-row-to-int)))
 
 (defn greatest-prod-in-row
   [row]
@@ -24,6 +32,10 @@
 
 (defn diagnols [grid]
   grid)
+
+(defn greatest-prod-adj [grid]
+  (max (greatest-prod-adj-horizontally grid) (greatest-prod-adj-vertically grid)))
+
 
 
 
