410d7ed93580d19f50e64bd8b10c7e72d4e9ea88
Tj Campanella <tj@arete.health>
2022-11-03 22:53:40 -0400

    wro

--- e11/src/e11/core.clj
+++ e11/src/e11/core.clj
@@ -22,6 +22,29 @@
     []
     (apply mapv vector grid)))
 
+(defn top-diagnols [grid]
+  (loop [rem grid res [] curr [] idx 0 iter 0]
+    (cond
+      (empty? (flatten rem)) res
+      (= idx (- (count grid) iter)) (recur (map #(rest %) rem) (conj res curr) [] 0 (inc iter))
+      :else (let [row (nth rem idx)]
+              (recur rem res (conj curr (nth row idx)) (inc idx) iter)))))
+
+(defn bottom-diagnols [grid]
+  (loop [rem grid res [] curr [] idx (dec (count grid)) iter 0]
+    (cond
+      (empty? (flatten rem)) res
+      (= idx -1) (recur (map #(rest %) rem) (conj res curr) [] (- (dec (count grid)) (inc iter)) (inc iter))
+      :else (let [row (nth rem idx)]
+              (recur rem res (conj curr (nth row idx)) (dec idx) iter)))))
+
+(defn diagnols [grid]
+  (let [top-left (top-diagnols grid)
+        top-right (top-diagnols (reverse grid))
+        bottom-left (bottom-diagnols grid)
+        bottom-right (bottom-diagnols (reverse grid))]
+    (distinct (remove empty? (concat top-left top-right bottom-left bottom-right)))))
+
 (defn greatest-prod-adj-horizontally [grid]
   (let  [res (map greatest-prod-in-row grid)]
     (if (empty? res) 0 (apply max res))))
@@ -30,12 +53,18 @@
   (let  [res (map greatest-prod-in-row (transpose grid))]
     (if (empty? res) 0 (apply max res))))
 
-(defn diagnols [grid]
-  grid)
+(defn greatest-prod-adj-diagnolly [grid]
+  (let  [res (map greatest-prod-in-row (diagnols grid))]
+    (if (empty? res) 0 (apply max res))))
 
 (defn greatest-prod-adj [grid]
-  (max (greatest-prod-adj-horizontally grid) (greatest-prod-adj-vertically grid)))
+  (max (greatest-prod-adj-horizontally grid)
+       (greatest-prod-adj-vertically grid)
+       (greatest-prod-adj-diagnolly grid)))
 
 
+(greatest-prod-adj-horizontally data)
+(greatest-prod-adj-vertically data)
+(greatest-prod-adj-diagnolly data)
 
-
+(diagnols data)
