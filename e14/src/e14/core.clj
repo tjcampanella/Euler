c9d0298d5e62076ae3ee8bb594a8da1c0ba1c4e2
Tj Campanella <tj@arete.health>
2022-11-10 21:33:20 -0500

    Optimized e14 a bit

--- e14/src/e14/core.clj
+++ e14/src/e14/core.clj
@@ -1,20 +1,24 @@
 (ns e14.core "Longest Collatz Sequence")
 
 (defn collatz
-  ([start]
-   (collatz start []))
-  ([curr res]
+  ([start] (collatz start [] {}))
+  ([start memory]
+   (collatz start [] memory))
+  ([curr res memory]
    (cond
+     (contains? memory curr) (concat res (memory curr))
      (= curr 1) (conj res 1)
-     (even? curr) (collatz (/ curr 2) (conj res curr))
-     :else (collatz (inc (* 3 curr)) (conj res curr)))))
+     (even? curr) (collatz (/ curr 2) (conj res curr) memory)
+     :else (collatz (inc (* 3 curr)) (conj res curr) memory))))
 
 (defn find-longest-collatz [limit]
-  (loop [curr-max 1 start-max 1 vals (range 1 limit)]
-    (let [res (collatz (first vals))
-          length (count res)]
+  (loop [curr-max 1 start-max 1 vals (range 1 limit) memory {}]
+    (let [res (collatz (first vals) memory)
+          length (count res)
+          new-memory (assoc memory (first vals) res)]
       (cond
         (empty? (rest vals)) [start-max curr-max]
-        (> length curr-max) (recur length (first vals) (rest vals))
-        :else (recur curr-max start-max (rest vals))))))
+        (> length curr-max) (recur length (first vals) (rest vals) new-memory)
+        :else (recur curr-max start-max (rest vals) new-memory)))))
 
+;; (time (find-longest-collatz 1000000))
